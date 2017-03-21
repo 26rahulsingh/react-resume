391f96509c52912e04edeba68ef88d1a97aae3e8
John Williamson <johnwilliamson@Johns-MacBook-Air-5.local>
2017-03-21 14:15:27 +1100

    Removed junk

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
           <Education schools={json.education} />
           <Skills skills={json.skills} />
           <References references={json.references} />
-          </div>
+        </div>
     </div>
     <Footer />
   </div>
